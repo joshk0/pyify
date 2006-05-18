4cb887b26605ae3ac56bcd28b279a993ef4655cb
Brandon Lewis <dotsony@gmail.com>
2006-05-18 05:54:09 +0000

    Implemented oggVorbis decode and encode module but there are some issues -- the tags get really screwed up and the field names are not uppercae (wtf?)
    it's a start...
    
    
    git-svn-id: file:///home/joshk/src/pyify.new/trunk@2 96c6e91c-0814-0410-bb7e-c4cc89681474

--- ify.py
+++ ify.py
@@ -5,45 +5,69 @@ import sys
 import string
 
 class audioFile:
-	"""Represents an abstraction of an audio file"""
-		
+	"""Represents an abstraction of an audio file. Don't
+	instantiate this class directly. Instead instantiate
+	one of its subclasses.
+	"""
+	
+	# use this constructor to instantiate a file in the
+	# source format. metadata is initialized from file tags
+	# using whatever method is appropriate.
 	def __init__(self, path):
 		self.path     = path
-		self.name     = os.basename(path)
-		(base, ext)   = os.split(self.basename)
+		(base, ext)   = os.path.splitext(os.path.basename(path))
 		self.type     = ext
+		self.name     = base
 		self.metadata = self.getMetaData()
 	
-	def __init__(self, path, metadata):
-		self.path     = path
-		self.name     = os.basename(path)
-		(base, ext)   = os.split(self.basename)
-		self.metadata = metadata
+	#default method simply returns the open file
+	def getAudioStream():
+		return open(path, "r")
 		
+# reference audioFile implementation class
 class oggFile(audioFile):
-
-	# return dictionary of file metadata
-	def getMetaData(self):
-		command = "vorbiscomment -l \"%s\"" % self.path
-		tags = popen(comand).readlines()
-		tags = [(x[0], x[1]) for x in [x.split("=") in tags]]
-		tags = dict(tags)
-		return dict
+	"""ogg/vorbis module"""
 	
-	# return open file object with audio stream
-	def getAudioStream(self):
-		command = "-d wav -q -f - %s" % self.path
-		return os.popen(command)
-
-	def encodeAudioStream(self):
-		flags = { 
+	#todo -- read this in from config file
+	encode_command = "oggenc -q4.5 -Q - %s -o %s"
+	
+	flags = { 
 			"ARTIST" : "-a", 
 			"TITLE" : "-t" ,
 			"ALBUM" : "-l" ,
 			"DATE" : "-d" ,
 		    "GENRE" : "-G", 
-		    "TRACKNUMBER" : "-N" 
-		}
-	"
+		    "TRACKNUMBER" : "-N"
+	}
+	
+	# return dictionary of file metadata
+	# key names are based on output of vorbiscomment
+	def getMetaData(self):
+		command = "vorbiscomment -l \"%s\"" % self.path
+		tags = os.popen(command).readlines()
+		tags = [(x[0], x[1]) for x in [x.split("=") for x in tags]]
+		tags = dict(tags)
+		return tags
+	
+	# return open file object with audio stream
+	def getAudioStream(self):
+		command = "ogg123 -d wav -q -f - %s" % self.path
+		return os.popen(command)
+
+	def encodeAudioStream(self, input_stream, destination):
+		tagargs = str()
 		for (key, value) in self.metadata.items():
-			
+			if self.flags.has_key(key):
+				tagargs = tagargs + "%s \"%s\" " % (self.flags[key], value)
+		command = self.encode_command % (tagargs, destination)
+		print "debug: " + command
+		
+		(child_stdin, child_stdout) = os.popen2(command)
+		child_stdin.write(input_stream.read())
+		child_stdin.close()
+
+#test
+theFile = oggFile("01.Les_Seigneurs.ogg")
+audio = theFile.getAudioStream()
+theFile.encodeAudioStream(audio, "test.ogg")
+
